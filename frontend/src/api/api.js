2ab7ec29d4080b3cf235eba0b07305ee2eeb8267
sriswasthik <sriswasthik006@gmail.com>
2026-03-19 22:49:07 +0530

    Change 3

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://invoice-generator-01s4.onrender.com"
+  baseURL: "https://invoice-generator-01s4.onrender.com/api"
 });
 
 export default api;
